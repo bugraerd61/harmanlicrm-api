101402f8af2ac69cf8b24e83f4b3a3ce5e33aa8f
bugraerd61 <57588606+bugraerd61@users.noreply.github.com>
2026-04-19 19:06:36 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,15 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), {
+  setHeaders: function(res, filePath) {
+    if (filePath.endsWith('index.html')) {
+      res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate');
+      res.setHeader('Pragma', 'no-cache');
+      res.setHeader('Expires', '0');
+    }
+  }
+}));
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
